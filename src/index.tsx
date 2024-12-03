0138dcece4f159d77e7c6e61659f5b54319db3a8
Artem Bykov <bykovaa94@mail.ru>
2024-12-03 16:07:01 +0300

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './configs/router';
 import { AuthProvider } from './context/contextProviders/AuthProvider';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './store/store';
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
-    <RouterProvider router={router} />
+    <ReduxProvider store={store}>
+      <RouterProvider router={router} />
+    </ReduxProvider>
   </AuthProvider>
 );
